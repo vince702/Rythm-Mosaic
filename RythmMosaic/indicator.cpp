35375d937d8f42470b0692e131c32083013faafb
Vincent Chi <vincentchi@s-169-232-120-232.resnet.ucla.edu>
2017-05-28 22:06:03 -0700

    changed indicator duration

--- RythmMosaic/indicator.cpp
+++ RythmMosaic/indicator.cpp
@@ -1,6 +1,24 @@
 #include "indicator.h"
+#include <QTimer>
 
-indicator::indicator()
-{
+indicator::indicator(int x, int y, int width, int length){
+    setRect(x,y, width, length);
+
+    QTimer * timerIndicator = new QTimer;
+    connect(timerIndicator, SIGNAL(timeout()),this,SLOT(shrink()));
+    timerIndicator->start(400);
+
+}
+
+void indicator::shrink(){
+
+    countdown--;
+
+
+
+    setRect(x,y, width, length);
+
+
+    if (this->countdown = 0) delete this;
 
 }
--- RythmMosaic/indicator.h
+++ RythmMosaic/indicator.h
@@ -1,11 +1,28 @@
 #ifndef INDICATOR_H
 #define INDICATOR_H
 
+#include <QGraphicsEllipseItem>
+#include <QKeyEvent>
+#include <QGraphicsScene>
 
-class indicator
+class indicator: public QObject, public QGraphicsRectItem
 {
+
+    Q_OBJECT
+
 public:
-    indicator();
+    indicator(int x, int y, int width, int length);
+    int x= 0;
+    int y= 0;
+    int width=0;
+    int length=0;
+    int countdown = 100;
+
+public slots:
+    void shrink();
+
 };
+
+
 
 #endif // INDICATOR_H
