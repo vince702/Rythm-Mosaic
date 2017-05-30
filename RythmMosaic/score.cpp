62d3e61eb111ade1a9203a5d94e1556016faebdd
Vincent Chi <vincentchi@Vincents-MacBook-Pro-2.local>
2017-05-29 18:03:27 -0700

    added score system

--- RythmMosaic/score.cpp
+++ RythmMosaic/score.cpp
@@ -1,6 +1,47 @@
 #include "score.h"
+#include <QFont>
 
-score::score()
+score::score(QGraphicsItem * parent) : QGraphicsTextItem(parent)
 {
 
+ hitcount = 0;
+ currentCombo = 0;
+ acc = 0;
+
+ setPlainText(QString("HIT_COUNT: ") +  QString::number(hitcount) + "              "
+              + QString("COMBO: ") +  QString::number(currentCombo) + "              "
+              + QString("MAX COMBO: ") +  QString::number(acc));
+
+
+ setDefaultTextColor(Qt::red);
+ setFont(QFont("arial", 15));
+
+
 }
+
+void score::increase(){
+
+   hitcount++;
+   setPlainText(QString("HIT_COUNT: ") +  QString::number(hitcount) + "              "
+                + QString("CURRENT COMBO: ") +  QString::number(currentCombo) + "              "
+                + QString("MAX COMBO: ") +  QString::number(maxCombo));
+
+}
+
+void score::increaseCurrentCombo(){
+
+   currentCombo++;
+   if (currentCombo > maxCombo) maxCombo++;
+   setPlainText(QString("HIT_COUNT: ") +  QString::number(hitcount) + "              "
+                + QString("CURRENT COMBO: ") +  QString::number(currentCombo) + "              "
+                + QString("MAX COMBO: ") +  QString::number(maxCombo));
+
+}
+
+void score::resetCombo(){
+    currentCombo = 0;
+    setPlainText(QString("HIT_COUNT: ") +  QString::number(hitcount) + "              "
+                 + QString("CURRENT COMBO: ") +  QString::number(currentCombo) + "              "
+                 + QString("MAX COMBO: ") +  QString::number(maxCombo));
+}
+
--- RythmMosaic/score.h
+++ RythmMosaic/score.h
@@ -1,11 +1,20 @@
 #ifndef SCORE_H
 #define SCORE_H
+#include <QGraphicsTextItem>
 
-
-class score
+class score: public QGraphicsTextItem
 {
 public:
-    score();
+    score(QGraphicsItem * parent = 0);
+    int hitcount = 0;
+    int currentCombo = 0;
+    int maxCombo = 0;
+    int acc = 0;
+
+
+    void increase();
+    void increaseCurrentCombo();
+    void resetCombo();
 };
 
 #endif // SCORE_H
