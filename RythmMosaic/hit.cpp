4188d511c20872392b80e109a87563d86f01ef15
Vincent Chi <vincentchi@Vincents-MacBook-Pro-2.local>
2017-05-29 18:00:33 -0700

    deletes note when struck, updates score

--- RythmMosaic/hit.cpp
+++ RythmMosaic/hit.cpp
@@ -1,17 +1,35 @@
 #include "hit.h"
+#include "keyboard.h"
 #include <QGraphicsEllipseItem>
 #include <QTimer>
-
+#include "key.h"
 #include <QDebug>
+extern Keyboard * k;
+
 hit::hit(int x, int y, int width, int length){
     setRect(x,y,width, length);
 
     QTimer * timer = new QTimer;
     connect(timer, SIGNAL(timeout()),this,SLOT(end()));
-    timer->start(50);
+    timer->start(20);
 
 }
 
 void hit::end(){
+   QList<QGraphicsItem *> colliding_items = collidingItems();
+  for (int i = 0,n = colliding_items.size(); i < n; ++i){
+     if (typeid(*(colliding_items[i])) == typeid(key)){
+
+         k->scoreDisplay->increase();
+         k->scoreDisplay->increaseCurrentCombo();
+         delete colliding_items[i];
+         delete this;
+         return;
+
+
+     }
+
+   }
+   k->scoreDisplay->resetCombo();
    delete this;
 }
