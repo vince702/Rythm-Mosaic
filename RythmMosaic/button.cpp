ff6b3c03a6ce0e15c7e2dcc52ab406e231bc2745
Vincent Chi <vincentchi@s-169-232-120-130.resnet.ucla.edu>
2017-06-05 20:43:05 -0700

    button class for song buttons

--- RythmMosaic/button.cpp
+++ RythmMosaic/button.cpp
@@ -1,6 +1,39 @@
 #include "button.h"
+#include <QGraphicsTextItem>
+#include <QBrush>
 
-button::button()
+extern int songNumber;
+
+button::button(QString word, int trackNo, QGraphicsItem * parent) : QGraphicsRectItem(parent)
 {
+setRect(0,0,200,50);
+QBrush brush;
+brush.setStyle(Qt::SolidPattern);
+brush.setColor(Qt::darkMagenta);
+setBrush(brush);
+QGraphicsTextItem* text = new QGraphicsTextItem(word,this);
+int x = rect().width()/2 - text->boundingRect().width()/2;
+int y = rect().height()/2 - text->boundingRect().height()/2;
+text->setPos(x,y);
+setAcceptHoverEvents(true);
+trackNumber = trackNo;
+}
 
+void button::mousePressEvent(QGraphicsSceneMouseEvent * event){
+    emit clicked();
+}
+
+void button::hoverEnterEvent(QGraphicsSceneHoverEvent *event){
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::red);
+    setBrush(brush);
+    songNumber = this->trackNumber;
+}
+
+void button::hoverLeaveEvent(QGraphicsSceneHoverEvent *event){
+    QBrush brush;
+    brush.setStyle(Qt::SolidPattern);
+    brush.setColor(Qt::darkMagenta);
+    setBrush(brush);
 }
